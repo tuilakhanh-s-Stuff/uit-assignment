9ebd8c6cfd6679d64331f42f61319940848db715
tuilakhanh <bacdau852@gmail.com>
2021-12-12 15:15:04 +0700

    09 - 11

---
+++ 09 MẢNG 1 CHIỀU/11.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+void inputList(int array[]) {
+  for (int i = 0; i < 10; i++) {
+    std::cin >> array[i];
+  }
+}
+
+int getFirstDigit(int n) {
+  while (n >= 10) {
+    n = n / 10;
+  }
+  return n;
+}
+
+bool isBenford(int array[]) {
+  int one = 0, four = 0;
+  int digit;
+  for (int i = 0; i < 10; i++) {
+    digit = getFirstDigit(array[i]);
+    if (digit == 1) {
+      one++;
+    } else if (digit == 4) {
+      four++;
+    }
+  }
+  if (one == 3 && four == 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+int main() {
+  int a[1000000];
+  inputList(a);
+  std::cout << (isBenford(a) ? "TRUE" : "FALSE");
+}
