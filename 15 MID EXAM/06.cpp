ea3b861ac2c0b7668c58d4226dabbf0dc7555125
tuilakhanh <bacdau852@gmail.com>
2022-01-06 09:28:31 +0700

    Update 15

---
+++ 15 MID EXAM/06.cpp
@@ -0,0 +1,23 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+class
+###End banned keyword*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+bool LaSNT(int n);
+int SNTthuN(int n);
+
+int main()
+{
+    int n;     
+    cin >> n; 
+    cout << SNTthuN(n) << endl;
+    return 0;
+}
+
+//###INSERT CODE HERE -
+
